dbae766da0faa4ab25b96a306fbb899d90194e2a
Rich240110 <richarddebarba@hotmail.com>
2025-06-15 20:11:11 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function Insta(){
+window.open("https://www.instagram.com/eebgiovanitrentini/", "_blank");
+}
+function Logo(){
+window.open("http://eebgiovanitrentini.blogspot.com/");
+}
+function Whats(){
+    window.open("https://wa.me/554733069633", "_blank")
+}
+function Maps(){
+    window.open("https://www.google.com.br/maps/place/EEB+Prof+Giovani+Trentini/@-26.7436941,-49.2781668,17z/data=!3m1!4b1!4m6!3m5!1s0x94defa07ae6e1289:0xf6560b7253e7dfb4!8m2!3d-26.7436941!4d-49.2755919!16s%2Fg%2F1twy_q2y?hl=pt-BR&entry=ttu&g_ep=EgoyMDI1MDYxMS4wIKXMDSoASAFQAw%3D%3D", "_blank")
+}
